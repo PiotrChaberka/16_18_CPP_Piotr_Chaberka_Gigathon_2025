68fa4f11b099f1a9915aaf8038ef2bd2f73d3ca3
EvixWasTaken <piotr5wwwwwz@gmail.com>
2025-05-01 22:07:32 +0200

    Major compatibility fix

---
+++ 16_18_CPP_Piotr_Chaberka/build/CMakeFiles/SolitaireGame.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SolitaireGame.dir/16_18_CPP_Piotr_Chaberka.cpp.obj"
+  "CMakeFiles/SolitaireGame.dir/16_18_CPP_Piotr_Chaberka.cpp.obj.d"
+  "SolitaireGame.exe"
+  "SolitaireGame.exe.manifest"
+  "SolitaireGame.pdb"
+  "libSolitaireGame.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SolitaireGame.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 16_18_CPP_Piotr_Chaberka/build/CMakeFiles/SolitaireGame.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for SolitaireGame.
+# This may be replaced when dependencies are built.
